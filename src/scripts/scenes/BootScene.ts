2e3a7b0323980c22e030ba02196cb6b3686b293c
phamhuuphuc <92615674+phamhuuphuc@users.noreply.github.com>
2022-08-01 16:41:36 +0700

    1/8 commit

--- src/scripts/scenes/BootScene.ts
+++ src/scripts/scenes/BootScene.ts
@@ -1,59 +1,59 @@
 export class BootScene extends Phaser.Scene {
-    private loadingBar: Phaser.GameObjects.Graphics
-    private progressBar: Phaser.GameObjects.Graphics
+  private loadingBar: Phaser.GameObjects.Graphics;
+  private progressBar: Phaser.GameObjects.Graphics;
 
-    constructor() {
-        super({
-            key: 'BootScene',
-        })
-    }
+  constructor() {
+    super({
+      key: "BootScene",
+    });
+  }
 
-    preload(): void {
-        this.load.pack('preload', './assets/pack.json', 'preload')
-        this.createLoadingbar()
-        this.showLoading()
-    }
+  preload(): void {
+    this.load.pack("preload", "./assets/pack.json", "preload");
+    this.createLoadingbar();
+    this.showLoading();
+  }
 
-    update(): void {
-        this.scene.start('MenuScene')
-    }
+  update(): void {
+    this.scene.start("MenuScene");
+  }
 
-    private createLoadingbar(): void {
-        this.loadingBar = this.add.graphics()
-        this.loadingBar.fillStyle(0x5dae47, 1)
-        this.loadingBar.fillRect(
-            this.cameras.main.width / 4 - 2,
-            this.cameras.main.height / 2 - 18,
-            this.cameras.main.width / 2 + 4,
-            20
-        )
-        this.progressBar = this.add.graphics()
-    }
+  private createLoadingbar(): void {
+    this.loadingBar = this.add.graphics();
+    this.loadingBar.fillStyle(0x5dae47, 1);
+    this.loadingBar.fillRect(
+      this.cameras.main.width / 4 - 2,
+      this.cameras.main.height / 2 - 18,
+      this.cameras.main.width / 2 + 4,
+      20
+    );
+    this.progressBar = this.add.graphics();
+  }
 
-    private showLoading() {
-        let _this = this
-        this.load.on(
-            'progress',
-            (value: number) => {
-                this.progressBar.clear()
-                this.progressBar.fillStyle(0xfff6d3, 1)
-                this.progressBar.fillRect(
-                    this.cameras.main.width / 4,
-                    this.cameras.main.height / 2 - 16,
-                    (this.cameras.main.width / 2) * value,
-                    16
-                )
-            },
-            this
-        )
+  private showLoading() {
+    let _this = this;
+    this.load.on(
+      "progress",
+      (value: number) => {
+        this.progressBar.clear();
+        this.progressBar.fillStyle(0xfff6d3, 1);
+        this.progressBar.fillRect(
+          this.cameras.main.width / 4,
+          this.cameras.main.height / 2 - 16,
+          (this.cameras.main.width / 2) * value,
+          16
+        );
+      },
+      this
+    );
 
-        this.load.on(
-            'complete',
-            () => {
-                this.progressBar.destroy()
-                this.loadingBar.destroy()
-            },
-            this
-        )
-    }
+    this.load.on(
+      "complete",
+      () => {
+        this.progressBar.destroy();
+        this.loadingBar.destroy();
+      },
+      this
+    );
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-    entry: './src/game.ts',
+    entry: './src/scripts/Game.ts',
     module: {
         rules: [
           {
